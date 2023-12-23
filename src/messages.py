472ee525f4b04efa63845d000300f7637e1bdbff
Alessandro Sorbello <a.sorbello@redraion.com>
2023-12-23 19:05:16 +0100

    feat: add real-time message handling for channel updates

--- src/messages.py
+++ src/messages.py
@@ -1,17 +1,11 @@
 from telethon.sync import TelegramClient, events
 from settings import *
 
-#client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=TOKEN)
-client = TelegramClient('prova', API_ID, API_HASH).start(phone=PHONE_NUMBER)
+# client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=TOKEN)
+client = TelegramClient("prova", API_ID, API_HASH).start(phone=PHONE_NUMBER)
 
-#@client.on(events.NewMessage(chats = CHANNELS))
-#async def main(event):
-#     me = client.get_me()
-#     print(me.stringify())
-#     print(event.stringify())
 
 async def get_channel_messages():
-    # Sostituisci 'channel_username' con il nome utente del canale (ad esempio, '@example_channel')
     channel_entity = await client.get_entity(CHANNELS[0])
 
     # Ottieni gli ultimi 10 messaggi dal canale
@@ -20,5 +14,11 @@ async def get_channel_messages():
     for message in messages:
         print(message.text)
 
-if __name__ == '__main__':
-    client.loop.run_until_complete(get_channel_messages())
+
+@client.on(events.NewMessage(chats=CHANNELS))
+async def handle_new_message(event):
+    print(event.message.text)
+
+
+if __name__ == "__main__":
+    client.run_until_disconnected()

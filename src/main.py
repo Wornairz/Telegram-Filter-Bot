ef28866f71e4e1b62de46f43f2fc0a35cd2f21fa
Alessandro Catalano <alessandrocatalano999@gmail.com>
2023-12-24 19:17:08 +0100

    feat: move channel filtering

--- src/main.py
+++ src/main.py
@@ -25,16 +25,17 @@ def main() -> None:
     application.run_polling()
 
 
-@client.on(events.NewMessage(chats=CHANNELS))
+@client.on(events.NewMessage)
 async def handle_new_message(event: Message):
-    channel_username = event.chat.username
+    channel_username = event.chat.username if event.chat is not None else event.chat_id
     message_id = event.message.id
-    for keyword in KEYWORDS:
-        if keyword.lower() in event.text.lower():
-            for user in USERS:
-                message_title = f"Trovata corrispondenza con la keyword <b>{keyword}</b> nel canale <i>{event.chat.title}</i>"
-                message_link = f"https://t.me/{channel_username}/{message_id}"
-                await application.bot.send_message(chat_id=user, text=message_title + "\n\n" + message_link, parse_mode=ParseMode.HTML)
+    if channel_username in CHANNELS:
+        for keyword in KEYWORDS:
+            if keyword.lower() in event.text.lower():
+                for user in USERS:
+                    message_title = f"Trovata corrispondenza con la keyword <b>{keyword}</b> nel canale <i>{event.chat.title}</i>"
+                    message_link = f"https://t.me/{channel_username}/{message_id}"
+                    await application.bot.send_message(chat_id=user, text=message_title + "\n\n" + message_link, parse_mode=ParseMode.HTML)
 
 
 if __name__ == "__main__":

36d31af0d359c41416b378907b5713ba9d5d8760
Alessandro Catalano <alessandrocatalano999@gmail.com>
2023-12-24 18:16:29 +0100

    format code

--- src/main.py
+++ src/main.py
@@ -18,6 +18,7 @@ async def start(update: Update, context: CallbackContext) -> None:
         "Ciao! Sono il tuo bot. Sono in ascolto su diversi canali."
     )
 
+
 def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
@@ -25,9 +26,9 @@ def main() -> None:
 
 @client.on(events.NewMessage(chats=CHANNELS))
 async def handle_new_message(event: Message):
-    #from_chat_id=event.chat_id
+    # from_chat_id=event.chat_id
     channel_username = event.chat.username
-    message_id=event.message.id
+    message_id = event.message.id
     for user in USERS:
         message_link = f"https://t.me/{channel_username}/{message_id}"
         await application.bot.send_message(chat_id=user, text=message_link)

f06ded095dd39a07dff0eca8b993b609ef77378d
Alessandro Sorbello <soralex008@gmail.com>
2023-12-25 23:28:29 +0100

    feat: init conversation handlers

---
+++ src/handlers.py
@@ -0,0 +1,86 @@
+import logging
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    ConversationHandler,
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+ADD_CHANNELS, REMOVE_CHANNELS, ADD_KEYWORDS, REMOVE_KEYWORDS = range(4)
+
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text(
+        "Ciao! Sono il tuo bot. Sono in ascolto su diversi canali."
+    )
+
+async def add_channels(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "Inserisci i canali che vorresti tracciare. Quando hai finito usa /stop."
+    )
+    return ADD_CHANNELS
+
+    
+async def add_channels_message(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "Canale aggiunto alla lista"
+    )
+    return ADD_CHANNELS
+
+async def remove_channels(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "Rimuovi i canali che non vuoi più tracciare. Quando hai finito usa /stop."
+    )
+    return REMOVE_CHANNELS
+
+async def remove_channels_message(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "Canale rimosso"
+    )
+    return REMOVE_CHANNELS
+
+async def add_keywords(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "Inserisci le keywords che vorresti tracciare. Quando hai finito usa /stop."
+    )
+    return ADD_KEYWORDS
+
+    
+async def add_keywords_message(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "keyword aggiunta alla lista"
+    )
+    return ADD_KEYWORDS
+
+async def remove_keywords(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "Rimuovi le keywords che non vuoi più tracciare. Quando hai finito usa /stop."
+    )
+    return REMOVE_KEYWORDS
+
+async def remove_keywords_message(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text(
+        "keyword rimossa"
+    )
+    return REMOVE_KEYWORDS
+
+async def stop_interact(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Ok, basta"
+    )
+    return ConversationHandler.END
--- src/main.py
+++ src/main.py
@@ -1,27 +1,45 @@
-from telegram import Update
 from telegram.constants import ParseMode
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    CallbackContext,
-)
+from telegram.ext import Application, CommandHandler
 from settings import TOKEN, API_ID, API_HASH, PHONE_NUMBER, CHANNELS, USERS, KEYWORDS
 from telethon.sync import TelegramClient, events
 from telethon.tl.custom import Message
+from handlers import *
 
 client = TelegramClient("bot", API_ID, API_HASH).start(phone=PHONE_NUMBER)
 application = Application.builder().token(TOKEN).build()
 
 
-async def start(update: Update, context: CallbackContext) -> None:
-    print(update.effective_chat.id)
-    await update.message.reply_text(
-        "Ciao! Sono il tuo bot. Sono in ascolto su diversi canali."
-    )
-
-
 def main() -> None:
     application.add_handler(CommandHandler("start", start))
+    
+    add_channel_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("add_channels", add_channels)],
+        states={ADD_CHANNELS: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_channels_message)]},
+        fallbacks=[CommandHandler("stop", stop_interact)],
+    )
+
+    remove_channel_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("remove_channels", remove_channels)],
+        states={REMOVE_CHANNELS: [MessageHandler(filters.TEXT & ~filters.COMMAND, remove_channels_message)]},
+        fallbacks=[CommandHandler("stop", stop_interact)],
+    )
+
+    add_keywords_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("add_keywords", add_keywords)],
+        states={ADD_KEYWORDS: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_keywords_message)]},
+        fallbacks=[CommandHandler("stop", stop_interact)],
+    )
+
+    remove_keywords_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("remove_keywords", remove_keywords)],
+        states={REMOVE_KEYWORDS: [MessageHandler(filters.TEXT & ~filters.COMMAND, remove_keywords_message)]},
+        fallbacks=[CommandHandler("stop", stop_interact)],
+    )
+
+    application.add_handler(add_channel_conv_handler)
+    application.add_handler(remove_channel_conv_handler)
+    application.add_handler(add_keywords_conv_handler)
+    application.add_handler(remove_keywords_conv_handler)
     application.run_polling()
 
 
@@ -35,7 +53,11 @@ async def handle_new_message(event: Message):
                 for user in USERS:
                     message_title = f"Trovata corrispondenza con la keyword <b>{keyword}</b> nel canale <i>{event.chat.title}</i>"
                     message_link = f"https://t.me/{channel_username}/{message_id}"
-                    await application.bot.send_message(chat_id=user, text=message_title + "\n\n" + message_link, parse_mode=ParseMode.HTML)
+                    await application.bot.send_message(
+                        chat_id=user,
+                        text=message_title + "\n\n" + message_link,
+                        parse_mode=ParseMode.HTML,
+                    )
 
 
 if __name__ == "__main__":
